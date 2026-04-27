13f01707912f6e1c198acd35ecddcab7b10b517a
ana12maria <creacana@gmail.com>
2026-04-27 20:57:36 +0300

    Fix syntax error in ScheduleManager

--- UCSS.Core/ScheduleManager.cs
+++ UCSS.Core/ScheduleManager.cs
@@ -3,14 +3,11 @@
     /* Aceasta clasa gestioneaza logica de verificare a suprapunerilor in orar */
     public class ScheduleManager
     {
-        /* Regula 8 din SRS: Declararea functiei inainte de utilizare (daca era interfata, dar aici o scriem direct) */
         /* Functie care verifica daca un profesor este deja ocupat intr-un anumit interval */
         public bool DetectTeacherConflict(int teacherId, string day, int timeSlot)
         {
-            /* Regula 10: Initializarea obligatorie a variabilelor cu o valoare de pornire */
+            /* Initializarea obligatorie a variabilelor cu o valoare de pornire */
             bool hasConflict = false;
-
-            /* Aici ar veni logica reala de cautare in baza de date */
 
             /* Regula 1: Acolade obligatorii la IF, chiar si pentru o singura linie */
             /* Regula 5: Este interzisa folosirea comenzii switch, folosim if-else */
